d137e1daec8be85ce5fb7c9139b33db46dae7f2e
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-26 19:04:18 +0700

    update controller payment

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -6,10 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  Res,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import type { Response } from 'express';
 
-@Controller('payments')
+@Controller('payments/momo')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post(':orderId')
+  async createPayment(@Param('orderId') orderId: string) {
+    return this.paymentsService.createMomoPayment(orderId);
+  }
+  @Post('ipn')
+  async ipn(@Body() body: any) {
+    return this.paymentsService.handleMomoIpn(body);
+  }
+  @Get('return')
+  async momoReturn(@Query() query: any, @Res() res: Response) {
+    return res.json({
+      message: 'Momo redirected back.',
+      data: query,
+    });
+  }
 }
